4a04e3bc63cbc572c5f796bb111e9481ae1ca75a
Luiz Henrique <45173371+luiizHenCode@users.noreply.github.com>
2021-11-21 12:16:45 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import QRCode, { CanvasQRCodeProps } from "qrcode.react";
+
+import styles from "./styles/App.module.scss";
 
 function App() {
+  const linkRef = useRef<any>();
+
+  const [code, setCode] = useState<string>("");
+
+  const handleDownloadQRcode = () => {
+    const canvas = document.querySelector(
+      `.${styles.QrCodeWrapper} > canvas`
+    ) as HTMLCanvasElement;
+
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL();
+    link.download = "QRCODE.png";
+    link.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h2>QRCode Generator</h2>
+
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          placeholder="Insira o texto aqui..."
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+      </div>
+
+      {code.length > 0 && (
+        <div className={styles.codeContainer}>
+          <div className={styles.QrCodeWrapper}>
+            <QRCode value={code} renderAs={"canvas"} size={400} />
+          </div>
+          <button
+            type="button"
+            className={styles.btnDownloadQRcode}
+            onClick={handleDownloadQRcode}
+          >
+            Baixar QRCODE
+          </button>
+        </div>
+      )}
     </div>
   );
 }
